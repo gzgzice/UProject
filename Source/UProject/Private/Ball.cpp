21331ff112918891f4b99ee29316bda4cfd6f84c
myoungjelee <mjzizou@naver.com>
2023-02-17 18:29:04 +0900

    .

--- Source/UProject/Private/Ball.cpp
+++ Source/UProject/Private/Ball.cpp
@@ -49,14 +49,14 @@ void ABall::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	startPos = FVector(-1350.0f, -1200.0f, 150.0f);
-	endPos = FVector(800.0f, 1600.0, 2850.0);
-
-	float x = FMath::Clamp(GetActorLocation().X, startPos.X, endPos.X);
-	float y = FMath::Clamp(GetActorLocation().Y, startPos.Y, endPos.Y);
-	float z = FMath::Clamp(GetActorLocation().Z, startPos.Z, endPos.Z);
-	FVector clampLoc = FVector(x, y, z);
-	SetActorLocation(clampLoc);
+// 	startPos = FVector(-1350.0f, -1200.0f, 150.0f);
+// 	endPos = FVector(800.0f, 1600.0, 2850.0);
+// 
+// 	float x = FMath::Clamp(GetActorLocation().X, startPos.X, endPos.X);
+// 	float y = FMath::Clamp(GetActorLocation().Y, startPos.Y, endPos.Y);
+// 	float z = FMath::Clamp(GetActorLocation().Z, startPos.Z, endPos.Z);
+// 	FVector clampLoc = FVector(x, y, z);
+// 	SetActorLocation(clampLoc);
 }
 
 void ABall::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
--- Source/UProject/Public/Ball.h
+++ Source/UProject/Public/Ball.h
@@ -10,8 +10,8 @@ UCLASS()
 class UPROJECT_API ABall : public AActor
 {
 	GENERATED_BODY()
-
-public:
+	
+public:	
 	// Sets default values for this actor's properties
 	ABall();
 
@@ -19,17 +19,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:
+public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 public:
 
 	UPROPERTY(Editanywhere)
-		class USphereComponent* ball;
+	class USphereComponent* ball;
 
 	UPROPERTY(EditAnywhere)
-		class UStaticMeshComponent* mesh;
+	class UStaticMeshComponent* mesh;
+
+	UPROPERTY(EditAnywhere)
+		FVector	startPos;
+
+	UPROPERTY(EditAnywhere)
+		FVector	endPos;
 
 	UFUNCTION()
 		void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
